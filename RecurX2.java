75035d0652c76c05f3d199b0acb3c591b57133a7
Jun_Water <81067832+tnwnsrla@users.noreply.github.com>
2021-05-08 15:25:06 +0900

    재귀를 제거한 recur 메서드입니다.
    https://blog.naver.com/tnwnsrla
    
    [재귀 알고리즘의 비재귀적 표현] - recur 메서드를 재귀 호출을 사용하지 않고 구현하는 방법
    
    1. 꼬리 재귀의 제거
    
     1) 메서드의 꼬리에서 재귀 호출하는 메서드 recur(n-2) 라는 말은 '인자로  n-2를 전달하여 recur 메서드를 
    
        호출한다'는 의미입니다. 따라서 이 호출은 아래처럼 바꿀 수 있습니다.
    
      2) n의 값을 n-2로 업데이트 하고 메서드의 시작 지점으로 돌아갑니다.
    
    2. 재귀의 제거
    
     1) 꼬리 재귀와 달리, 앞에서 호출한 재귀 메서드의 제거는 쉽지 않습니다. 왜냐하면 변수 n의 값을 출력하기 전에
    
       recur(n-1)을 먼저 수행해야 하기 때문입니다.
    
     2) 즉, 현재 n의 값을 '잠시' 저장하고 recur(n-1)의 처리가 완료된 다음에 n의 값을 출력할 때는 '저장했던 n을 다시
    
       꺼내 그 값을 출력합니다.'

---
+++ RecurX2.java
@@ -0,0 +1,29 @@
+package chap04;
+
+class RecurX2 {
+	static void recur(int n) {
+		IntStack s = new IntStack(n);
+		
+		while(true) {
+			if(n > 0) {
+				s.push(n); // n의 값을 푸시
+				n = n -1; 
+				continue;
+			}
+			if(s.isEmpty() != true) { // 스택이 비어 있지 않다면
+				n = s.pop(); 		 // 저장하고 있던 스택의 값을 팝
+				System.out.println(n);
+				n = n - 2;
+				continue;
+			}
+			break;
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		recur(4);
+
+	}
+
+}
