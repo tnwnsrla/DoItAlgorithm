bed663145f6115acd16870be6bd597cdab4ca8cb
Jun_Water <81067832+tnwnsrla@users.noreply.github.com>
2021-05-20 22:27:46 +0900

    집합의 다양한 메소드들입니다.
    max
    집합의 최대 크기를 나타내는 필드입니다.
    
    num
    집합의 요소개수입니다. 집합의 요소는 배열의 앞쪽부터 0~num-1까지 넣습니다.
    ​
    set
    집합을 저장할 배열입니다. 배열의 메모리 확보는 생성자에서 합니다.
    
    생성자 IntSet
    집합은 처음에는 비어있기 때문에 0을 대입합니다.
    생성자의 매개변수 capacityh를 max 에 복사하고 집합의 요소 개수가 max가 되도록 배열 set을 생성합니다.
    
    집합의 최대 개수를 반환하는 capacity 메서드
    집합이 가질 수 있는 가장 큰 값을 반환하는 메서드로, max의 값을 반환합니다.
    
    집합의 현재 요소 개수를 반환하는 size 메서드
    집합의 현재 요소 개수를 반환하는 메서드로, num의 값을 반환합니다.
    
    요소를 검색하는 indexOf 메서드
    집합 배열 set에 n이 있는지 선형 검색으로 확인합니다.
    성공하면 찾은 요소 n의 인덱스를 반환하고, 실패하면 -1을 반환합니다.
    
    요소 n이 있는지 확인하는 contains 메서드
    집합 배열 set에 n이 들어 있는지 확인하는 메서드입니다.
    
    요소 n을 집합에 추가하는 add메서드
    배열이 비어 있고 집합에 같은 요소가 들어 있지 않은 경우에만 요소를 추가합니다.
    
    요소 n을 집합에서 삭제하는 remove 메서드
    집합에서 요소 n을 삭제하는 메서드입니다.

---
+++ IntSet.java
@@ -0,0 +1,67 @@
+package chap07;
+
+public class IntSet {
+	private int max; // 집합의 최대 개수
+	private int num; // 집합의 요소 개수
+	private int[] set; // 집합 본체
+	
+	// 생성자
+	public IntSet(int capacity) {
+		num = 0;
+		max = capacity;
+		try {
+			set = new int[max]; // 집합 배열 생성
+		} catch(OutOfMemoryError e) { // 배열 생성실패
+			max = 0;
+		}
+	}
+	
+	// 집합의 최대 개수
+	public int capacity() {
+		return max;
+	}
+	
+	// 집합의 요소 개수
+	public int size() {
+		return num;
+	}
+	
+	//집합에서 n을 검색합니다.(index 반환)
+	public int indexOf(int n) {
+		for(int i = 0; i < num; i++) {
+			if(set[i] == n) {
+				return i; // 검색성공
+			}
+		}
+		return -1; //검색실패
+	}
+	
+	// 집합에 n이 있는지 없는지 확인합니다.
+	public boolean contains(int n ) {
+		return(indexOf(n) != -1) ? true : false;
+	}
+	
+	// 집합에 n을 추가합니다.
+	public boolean add(int n) {
+		if(num >= max || contains(n) == true) { // 가득 찼거나 이미 n이 존재합니다.
+			return false;
+		} else {
+			set[num++] = n; // 가장 마지막 자리에 추가합니다.
+		}
+		return true;
+	}
+	
+	//집합에서 n을 삭제합니다.
+	public boolean remove(int n) {
+		int idx; // n이 저장된 요소의 인덱스
+		
+		if(num <= 0 || (idx = indexOf(n)) == -1) { // 비어 있거나 n이 존재하지 않습니다.
+			return false;
+		} else {
+			set[idx] = set[--num]; // 마지막 요소를 삭제한 곳으로 옮깁니다.
+		}
+		
+		return true;
+	}
+
+}
